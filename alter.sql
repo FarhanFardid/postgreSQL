d28d64d91cf80ee5dbb9167c4d5a78ca7d1d739c
FarhanFardid <farhanfardid009@gmail.com>
2025-05-19 02:52:46 +0600

    DML alter keyword functionality

--- alter.sql
+++ alter.sql
@@ -1,1 +1,19 @@
+-- get  all table info
 SELECT * FROM person;
+
+-- add new table attribute column
+ALTER Table person ADD COLUMN email VARCHAR(25) DEFAULT 'default@gmail.com' NOT NULL;
+
+-- insert new values
+INSERT INTO person values(6, 'Richie', 'Rich', 21, 'rr@rich.com');
+
+-- Rename a column attribute name
+ALTER TABLE person RENAME COLUMN age TO person_age;
+
+-- Modify the attribute type 
+ALTER Table person ALTER COLUMN first_name TYPE VARCHAR(50);
+
+-- Modify the  constraint of column attribute
+ALTER Table person ALTER COLUMN person_age set NOT NULL;
+
+ALTER Table person ALTER COLUMN person_age DROP NOT NULL;
