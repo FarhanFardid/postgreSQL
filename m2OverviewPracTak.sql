808959db9028b3a12b9281bd2381c81dd5dd4469
FarhanFardid <farhanfardid009@gmail.com>
2025-06-16 00:23:43 +0600

    function, procedure, trigger and indexing fetaures practice task

--- m2OverviewPracTak.sql
+++ m2OverviewPracTak.sql
@@ -97,6 +97,160 @@ SELECT * from departments d WHERE EXISTS(
 );
 
 -- Q5: Create a view to show each student’s name, department, and score.
-CREATE VIEW student_info AS SELECT name, department_name , score from students s JOIN departments d ON s.department_id=d.department_id;
+CREATE VIEW student_info AS 
+SELECT name, department_name , score 
+from students s JOIN departments d ON s.department_id=d.department_id;
 
+-- Q6: Create a view that lists all students enrolled in any course with the enrollment date.
+
+CREATE VIEW enrolled_students AS
+SELECT  s.student_id,
+    s.name,
+    s.age,
+    s.score,
+    s.department_id,
+    c.course_title,
+    c.enrolled_on 
+    from  students s JOIN course_enrollments c ON s.student_id = c.student_id;
+
+-- Q7: Query from your created views to verify the data.
 SELECT * from student_info;
+SELECT * from enrolled_students;
+
+-- Q8: Create a function that takes a student's score and returns a grade (e.g., A, B, C, F).
+CREATE OR REPLACE FUNCTION grading_system(p_score NUMERIC)
+RETURNS VARCHAR
+LANGUAGE plpgsql
+AS $$
+DECLARE
+ grade VARCHAR;
+BEGIN
+IF p_score >= 90 THEN
+grade :='A';
+ELSEIF p_score >= 80 THEN
+grade :='B'; 
+ELSEIF p_score >= 70 THEN
+grade :='C';
+ELSE
+grade :='F';
+END IF;
+
+RETURN grade;
+END;
+$$;
+
+
+SELECT name, score, grading_system(score) AS grade
+FROM students;
+
+-- Q9: Create a function that returns the full name and department of a student by ID.
+CREATE or REPLACE FUNCTION student_detail(p_std_id INT)
+RETURNS  VARCHAR
+LANGUAGE plpgsql
+AS $$
+DECLARE 
+std_name VARCHAR;
+department_title VARCHAR;
+BEGIN
+SELECT s.name, d.department_name INTO std_name , department_title from students s JOIN departments d ON s.department_id = d.department_id where s.student_id = p_std_id;
+
+RETURN std_name || ' from department of - ' || department_title;
+END;
+$$;
+
+SELECT * from student_detail(5);
+
+-- Q10: Write a stored procedure to update a student's department
+CREATE PROCEDURE update_dept(p_std_id INT, p_dept_id INT)
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+UPDATE students 
+SET department_id = p_dept_id
+where student_id = p_std_id;
+END;
+$$;
+
+CALL update_dept(3,2);
+
+-- Q11: Write a procedure to delete students who haven't enrolled in any course.
+
+CREATE OR REPLACE PROCEDURE delete_students()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+DELETE from students where student_id NOT IN (SELECT student_id from course_enrollments );
+  RAISE NOTICE 'Deleted students who were not enrolled in any course.';
+END;
+$$; 
+
+CALL delete_students();
+
+-- Q12: Create a trigger that automatically logs enrollment when a student is added to course_enrollments.
+
+CREATE TABLE enrollment_logs (
+    log_id SERIAL PRIMARY KEY,
+    student_id INT,
+    course_title VARCHAR(50),
+    enrolled_on DATE,
+    log_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE OR REPLACE FUNCTION log_enrollment()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO enrollment_logs(student_id, course_title, enrolled_on)
+      VALUES (NEW.student_id, NEW.course_title, NEW.enrolled_on);
+    
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER trigger_log_enrollment
+AFTER INSERT ON course_enrollments
+FOR EACH ROW
+EXECUTE FUNCTION log_enrollment();
+
+INSERT INTO course_enrollments(student_id, course_title, enrolled_on)
+VALUES (5, 'Database Systems', CURRENT_DATE);
+
+-- Q13: Add a trigger that sets the score to 0 if a new student record is added without a score.
+CREATE OR REPLACE FUNCTION default_score()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN 
+IF New.score IS NULL THEN
+New.score := 0;
+END IF;
+RETURN New;
+END;
+$$;
+
+CREATE Trigger default_score_trigger
+BEFORE INSERT on students
+FOR EACH ROW
+EXECUTE FUNCTION default_score();
+
+INSERT INTO students (name, age, department_id) 
+VALUES ('Rafiul Islam', 22, 3);
+
+-- Q14: Add an index to the score column in the students table.
+
+CREATE INDEX idx_student_score on students (score);
+
+
+-- Q15: Add a composite index on student_id and enrolled_on in the course_enrollments table.
+
+CREATE INDEX idx_student_enrollment 
+ON course_enrollments(student_id, enrolled_on);
+
+-- Q16: Compare query performance with and without indexes using EXPLAIN
+-- Before indexing
+EXPLAIN ANALYSE SELECT * from students where age >= 20;
+-- After Indexing
+CREATE INDEX idx_student_age on students (age);
+
+EXPLAIN ANALYSE SELECT * from course_enrollments where enrolled_on ='2024-03-10' ;
